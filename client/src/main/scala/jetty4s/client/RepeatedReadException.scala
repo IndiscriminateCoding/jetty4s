4b3fbde2e006a5520ef8845cc4c9c8a8f12ec3c3
IndiscriminateCoding <28496046+IndiscriminateCoding@users.noreply.github.com>
2019-12-09 20:07:16 +0300

    RepeatedReadException is MessageFailure

--- client/src/main/scala/jetty4s/client/RepeatedReadException.scala
+++ client/src/main/scala/jetty4s/client/RepeatedReadException.scala
@@ -1,3 +1,16 @@
 package jetty4s.client
 
-object RepeatedReadException extends Exception("repeated stream read isn't allowed")
+import cats.Applicative
+import org.http4s._
+
+object RepeatedReadException extends MessageFailure {
+  val message: String = "repeated stream read isn't allowed"
+
+  val cause: Option[Throwable] = None
+
+  def inHttpResponse[F[_], G[_]](
+    httpVersion: HttpVersion
+  )(implicit F: Applicative[F], G: Applicative[G]): F[Response[G]] = F.pure(Response[G](
+    status = Status.InternalServerError
+  ))
+}
