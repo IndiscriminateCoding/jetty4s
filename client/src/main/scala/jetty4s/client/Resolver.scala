a1e272561e4e1a0775975a0265eb12f1aa57d341
IndiscriminateCoding <28496046+IndiscriminateCoding@users.noreply.github.com>
2020-04-27 20:15:50 +0300

    Fix 2.12 build

--- client/src/main/scala/jetty4s/client/Resolver.scala
+++ client/src/main/scala/jetty4s/client/Resolver.scala
@@ -6,7 +6,7 @@ import java.util
 import cats.effect.{ Effect, IO }
 import org.eclipse.jetty.util.{ Promise, SocketAddressResolver }
 
-import scala.jdk.CollectionConverters._
+import scala.collection.JavaConverters._
 
 trait Resolver[F[_]] {
   def resolve(host: String, port: Int): F[List[InetSocketAddress]]
